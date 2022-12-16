308afbec68c46d09db092eebb4f01fecf4d9f583
Tiago1214 <tiagoamaro021@gmail.com>
2022-12-16 12:27:59 +0000

    resolução de erro no frontend

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -99,6 +99,6 @@ class SiteController extends Controller
     public function actionLogout()
     {
         Yii::$app->user->logout();
-        return $this->redirect('../../../frontend/web/site/index');
+        return $this->redirect('http://front.test/site/index');
     }
 }
