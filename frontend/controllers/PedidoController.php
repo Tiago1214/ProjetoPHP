92670446d71507f2bc69c7517e9452bbf2a7b814
bernardo2211876 <92576479+bernardo2211876@users.noreply.github.com>
2023-01-06 09:42:09 +0000

    e

--- frontend/controllers/PedidoController.php
+++ frontend/controllers/PedidoController.php
@@ -10,6 +10,7 @@ use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use Yii;
 
 /**
  * PedidoController implements the CRUD actions for Pedido model.
